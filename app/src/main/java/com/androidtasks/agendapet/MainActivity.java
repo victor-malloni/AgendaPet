7f5fcd89017e29b7cbb5e24c146627ba3b4adcf2
victor-malloni <oxemalloni@gmail.com>
2022-09-07 05:14:13 -0300

    adicionando dependencias do androidX e criando um FAB

--- app/src/main/java/com/androidtasks/agendapet/MainActivity.java
+++ app/src/main/java/com/androidtasks/agendapet/MainActivity.java
@@ -1,14 +1,26 @@
 package com.androidtasks.agendapet;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public final class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab_adicionar = findViewById(R.id.fab_adicionar);
+        fab_adicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"test click button",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
