22640c4c708a1bddc954732fa936bd64c350131d
victor-malloni <oxemalloni@gmail.com>
2022-09-07 01:04:33 -0300

    inicializando projeto

---
+++ app/src/main/java/com/androidtasks/agendapet/MainActivity.java
@@ -0,0 +1,14 @@
+package com.androidtasks.agendapet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
